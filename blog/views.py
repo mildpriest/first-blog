69850ec10872e7b65a27a71ec13e430c8a2c7aa1
mildpriest <taeng2daa@naver.com>
2017-05-15 14:32:55 +0900

    update.
    add detail

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', {'posts': posts})
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail.html', {'post': post})

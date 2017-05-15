4565f92cc1266d56592d6df76bfa43e0413479a1
mildpriest <taeng2daa@naver.com>
2017-05-15 15:58:03 +0900

    리스트 정렬 추가

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from .porm import PostForm
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.order_by('-published_date')
     return render(request, 'blog/index.html', {'posts': posts})
 
 def detail(request, pk):

18fa318b5eea027de11d78d2f14a943a3ff95918
mildpriest <taeng2daa@naver.com>
2017-05-15 12:09:07 +0900

    3nd
    view update

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/index.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', {'posts': posts})

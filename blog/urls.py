c672cf95260dc73e1973df8cb8db8889454589ba
mildpriest <taeng2daa@naver.com>
2017-05-16 14:15:13 +0900

    메뉴 & 카테고리 적용

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^list/python/$', views.ctg_list_py, name='ctg_list_py'),
+    url(r'^list/java/$', views.ctg_list_java, name='ctg_list_java'),
+    url(r'^list/javascript/$', views.ctg_list_javascript, name='ctg_list_javascript'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.edit, name='edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,20 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .porm import PostForm
 
-
-# Create your views here.
-
 def post_list(request):
     posts = Post.objects.order_by('-published_date')
+    return render(request, 'blog/index.html', {'posts': posts})
+
+def ctg_list_py(request):
+    posts = Post.objects.filter(category='py').order_by('-published_date')
+    return render(request, 'blog/index.html', {'posts': posts})
+
+def ctg_list_java(request):
+    posts = Post.objects.filter(category='java+spring').order_by('-published_date')
+    return render(request, 'blog/index.html', {'posts': posts})
+
+def ctg_list_javascript(request):
+    posts = Post.objects.filter(category='javascript').order_by('-published_date')
     return render(request, 'blog/index.html', {'posts': posts})
 
 def detail(request, pk):

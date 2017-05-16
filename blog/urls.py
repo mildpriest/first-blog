75f98628a09c1e82699c7c1cee8f77bd6d583908
mildpriest <taeng2daa@naver.com>
2017-05-16 18:35:05 +0900

    코멘트 추가

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     url(r'^list/java/$', views.ctg_list_java, name='ctg_list_java'),
     url(r'^list/javascript/$', views.ctg_list_javascript, name='ctg_list_javascript'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.edit, name='edit'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+    url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,23 +3,24 @@ from django.utils import timezone
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 
-from .models import Post
-from .porm import PostForm
+from .models import Post, Comment
+from .porm import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
-    posts = Post.objects.order_by('-published_date')
+    posts = Post.objects.order_by('-created_date')
     return render(request, 'blog/index.html', {'posts': posts})
 
 def ctg_list_py(request):
-    posts = Post.objects.filter(category='py').order_by('-published_date')
+    posts = Post.objects.filter(category='py').order_by('-created_date')
     return render(request, 'blog/index.html', {'posts': posts})
 
 def ctg_list_java(request):
-    posts = Post.objects.filter(category='java+spring').order_by('-published_date')
+    posts = Post.objects.filter(category='java+spring').order_by('-created_date')
     return render(request, 'blog/index.html', {'posts': posts})
 
 def ctg_list_javascript(request):
-    posts = Post.objects.filter(category='javascript').order_by('-published_date')
+    posts = Post.objects.filter(category='javascript').order_by('-created_date')
     return render(request, 'blog/index.html', {'posts': posts})
 
 def detail(request, pk):
@@ -52,3 +53,28 @@ def edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/edit.html', {'form': form})
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('detail', pk=comment.post.pk)

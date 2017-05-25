25c3cdd8976647e0edebc5332c9e3f91a3200b68
mildpriest <taeng2daa@naver.com>
2017-05-25 15:39:10 +0900

    어드민 > 방력록 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Comment, Category
+from .models import Post, Comment, Category, Guest
 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Category)
+admin.site.register(Guest)
